35a62cbc7911360b3acd70d201130f8f33b78dd6
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2025-02-05 10:52:37 -0800

    rename Header test

--- components/layout/header.test.tsx
+++ components/layout/header.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Header from './header'
  
-test('Page', () => {
+test('Header', () => {
   render(<Header />)
   expect(screen.getByRole('heading', { level: 1, name: /Moving Pictures/i })).toBeDefined()
 })
